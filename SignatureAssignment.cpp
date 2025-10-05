b9c38770a6bbfd864745affa89c8c1b4793ba007
vwhernan <148294852+vwhernan@users.noreply.github.com>
2025-10-05 13:50:39 -0700

    Update SignatureAssignment.cpp
    Completed createArray and writeBinary functions

--- SignatureAssignment.cpp
+++ SignatureAssignment.cpp
@@ -1,27 +1,55 @@
 using namespace std;
 #include <iostream>
 #include <fstream>
-#include <cctype>
-#include <vector>
+#include <random>
+
+const int ARRAY_SIZE = 1000;
 
 int* createArray(int* length) {
+    *length = ARRAY_SIZE;
+    int* array = new int[ARRAY_SIZE];
 
+    for (int i = 0; i < 1000; i++) {
+        array[i] = rand() % 1000;
+    }
+    return array;
 }
-void writeBinary(int* values, int length) {
 
+void writeBinary(int* values, int length) {
+    string binarypath = "binary.dat";
+    ofstream File(binarypath, ios::binary);
+    
+    if (!File) {
+        cerr << "Error: Cannot open output file: " << binarypath << "\n";
+        return;
+    }
+
+    File << "The Length of the Array is: " << length << "\n";
+    for (int i = 0; i < length; i++) {
+        File << values[i] << " ";
+    }
+    File.close();
 }
 
 int* readBinary(int& length) {
-
+    return 0;
 }
 
 int main()
 {
+    //Declarations
+    int length = 0;
+   
     //Create Array
-
-    //Write Binary and delete array
-
+    int* integerArray = createArray(&length);
+ 
+    //Write Binary 
+    writeBinary(integerArray, length);
+    
     //Read Binary and Print
+
+    //Delete Arrays
+    
 }
 
 
